92b26246581721d2c6af927ee8b0e451b933c71f
Lucas <lupelikan@int.pl>
2025-03-06 00:31:23 +0200

    added wiremock for integration tests, and rest template, implemented scheduler and used it to fetch offers one time at 3 hours and tested it in integration test using awaitility library, added scheduler configuration (depending on property)

--- src/main/java/pl/joboffers/domain/offer/dto/JobOfferResponseDto.java
+++ src/main/java/pl/joboffers/domain/offer/dto/JobOfferResponseDto.java
@@ -1,8 +1,11 @@
 package pl.joboffers.domain.offer.dto;
 
+import lombok.Builder;
+
+@Builder
 public record JobOfferResponseDto(
-        String companyName,
-        String position,
+        String title,
+        String company,
         String salary,
         String offerUrl
 ) {
