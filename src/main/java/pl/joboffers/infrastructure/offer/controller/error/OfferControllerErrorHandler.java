9ec1387b3cb2386856aa6155018055b10f707158
Lucas <lupelikan@int.pl>
2025-03-10 21:49:25 +0200

    added offer duplicate handler when user wants to add offer that already exists (its url) but for now does not work because I use DuplicateKeyException from spring (I have to remove throwing my own exceptions for duplicate offers)

--- src/main/java/pl/joboffers/infrastructure/offer/controller/error/OfferControllerErrorHandler.java
+++ src/main/java/pl/joboffers/infrastructure/offer/controller/error/OfferControllerErrorHandler.java
@@ -1,12 +1,15 @@
 package pl.joboffers.infrastructure.offer.controller.error;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import pl.joboffers.domain.offer.OfferNotFoundException;
+
+import java.util.Collections;
 
 @ControllerAdvice
 @Slf4j
@@ -20,6 +23,18 @@ public class OfferControllerErrorHandler {
         return OfferErrorResponse.builder()
                 .message(errorMessage)
                 .status(HttpStatus.NOT_FOUND)
+                .build();
+    }
+
+    @ExceptionHandler(DuplicateKeyException.class)
+    @ResponseBody
+    @ResponseStatus(HttpStatus.CONFLICT)
+    public OfferPostErrorResponse getOfferDuplicateResponse(DuplicateKeyException duplicateKeyException) {
+        String message = duplicateKeyException.getMessage();
+        log.error(message);
+        return OfferPostErrorResponse.builder()
+                .errorMessages(Collections.singletonList(message))
+                .status(HttpStatus.CONFLICT)
                 .build();
     }
 }
